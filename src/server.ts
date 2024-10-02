52597c063387d181b6216fb53044751747aa0191
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-10-02 01:34:12 -0300

    first route test

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import Fastify from "fastify";
 
 const app = Fastify({ logger: true });
 
+app.get("/", (_, reply) => {
+  reply.status(200).send({
+    message: "First route bitches!"
+  })
+})
+
 const main = async () => {
   try {
     await app.listen({ port: Number(process.env.PORT) || 3333 });
