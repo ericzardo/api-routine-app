8c487404f8dc6a507fa97e5b53d153e3f4dcd63e
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-10-10 00:08:30 -0300

    create builder documents hierarchy function

---
+++ src/utils/buildDocumentHierarchy.ts
@@ -0,0 +1,38 @@
+import { Document as PrismaDocument } from "@prisma/client";
+
+type Document = PrismaDocument & { content: Document[] };
+
+const buildDocumentHierarchy = (documents: Document[]) => {
+  const map = new Map<string, Document>();
+  const root: Document[] = [];
+
+  documents.forEach((doc) => {
+    if (doc) {
+      map.set(doc.id, { ...doc, content: [] });
+    }
+  });
+
+  documents.forEach((doc) => {
+    if (doc) {
+      if (doc.parentId) {
+        const parent = map.get(doc.parentId);
+        if (parent) {
+          const currentDoc = map.get(doc.id);
+          if (currentDoc) {
+            parent.content.push(currentDoc);
+          }
+        }
+      } else {
+        const currentDoc = map.get(doc.id);
+        if (currentDoc) {
+          root.push(currentDoc);
+        }
+      }
+    }
+  });
+
+  console.log(JSON.stringify(root, null, 2));
+  return root;
+}
+
+export default buildDocumentHierarchy;
